65ef8fba861306d35165eb80b93a2530b730bdc3
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-06-16 13:24:25 -0700

    Implement Display and Error traits for Error

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use core::str::Utf8Error;
 
 #[cfg(feature = "std")]
@@ -12,6 +13,8 @@ pub enum Error {
     Utf8(Utf8Error),
 }
 
+static NUL_ERROR: &str = "Missing nul byte at the end of the string";
+
 impl From<Utf8Error> for Error {
     #[inline]
     fn from(err: Utf8Error) -> Error {
@@ -24,5 +27,33 @@ impl From<FromBytesWithNulError> for Error {
     #[inline]
     fn from(_: FromBytesWithNulError) -> Error {
         Error::Nul
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Nul => NUL_ERROR.fmt(f),
+            Error::Utf8(err) => err.fmt(f),
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl ::std::error::Error for Error {
+    #[inline]
+    fn description(&self) -> &str {
+        match *self {
+            Error::Nul => NUL_ERROR,
+            Error::Utf8(ref err) => err.description(),
+        }
+    }
+
+    #[inline]
+    fn cause(&self) -> Option<&::std::error::Error> {
+        match *self {
+            Error::Utf8(ref err) => Some(err),
+            _ => None,
+        }
     }
 }
